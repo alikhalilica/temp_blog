192df80db3039709a6a621d1fbb1bea91cc434ad
alikhalilica <samavat26>
2020-05-09 13:24:18 -0500

    added category field
    + category manytomany field
    + post single category added

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Category
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','auther','created_date']
-    list_filter = ("auther", 'created_date')
+    list_display = ['title','auther','created_date','get_category']
+    list_filter = ("auther", 'created_date','category')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+    def get_category(self, obj):
+        return "\n".join([p.name for p in obj.category.all()])
+
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title=models.CharField(max_length=200)
     slug=models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -8,7 +16,8 @@ class Post(models.Model):
     auther=models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     created_date=models.DateField(auto_now_add=True)
     image=models.ImageField(upload_to = "blog/" , blank=True)
-    #category
+    #category=models.ForeignKey(Category,on_delete=models.CASCADE,blank=True,null=True) #{{post.category.name}}
+    category=models.ManyToManyField(Category) #{% for  cat in post.category.all %} <li><a href="#">{{cat.name}}</a></li> {% endfor %}
     #tags
     #status
     #published_date
